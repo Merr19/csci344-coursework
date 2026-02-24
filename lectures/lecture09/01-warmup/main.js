462dd1cd1617b5beaa4a74ee206cdb0ec03ea609
Merr19 <Merrylynvc@gmail.com>
2026-02-24 00:04:50 -0500

    WIP

---
+++ lectures/lecture09/01-warmup/main.js
@@ -0,0 +1,39 @@
+let canvasWidth = document.documentElement.clientWidth - 10;
+let canvasHeight = document.documentElement.clientHeight - 10;
+let width = 10;
+
+// in p5.js, the function runs on page load:
+function setup() {
+    rectMode(CENTER);
+    createCanvas(canvasWidth, canvasHeight);
+}
+
+// in p5.js, special event handler that listens for click events:
+function mouseClicked() {
+    // in p5.js, mouseX and mouseY are
+    // built-in global variabls that track the
+    // current position of your mouse.
+    circle(mouseX, mouseY, width);
+}
+
+// in p5.js, special event handler that listens for drag events:
+function mouseDragged() {
+    // circle(mouseX, mouseY, Math.random() * 40 + 10);
+    fill("blue");
+    circle(mouseX, mouseY, 30);
+}
+
+// function mouseDragged() {
+//     circle(mouseX, mouseY, width);
+//     width += 1;
+// }
+
+/**
+ * Challenges:
+ * 1. As you click / drag, can the circles have different colors and sizes?
+ *      * Try using the Math.random() function
+ * 2. Can you make the click / drag sometimes make circles and sometimes make rectangles
+ *      * Sample rectangle function invocation: rect(mouseX, mouseY, 100, 100);
+ * 3. Can you make each click create a bulleye of different colors?
+ *      * Hint: make sure you draw the bigger circles before you draw the smaller circles.
+ */
---
+++ lectures/lecture09/02-style-property-demo/main.js
@@ -0,0 +1,39 @@
+
+const changeElementStyle = (newcolor, selector) =>{
+    let el = document.querySelector(selector);
+    el.style.backgroundColor = newcolor;
+    el.style.fontSize = '50px';
+
+};
+
+
+
+// const makeRed = () => {
+//     // your code here...
+//     console.log('Change background to red');
+//     document.querySelector('body').style.backgroundColor = 'red';
+//     document.querySelector('#section1').style.backgroundColor = 'red';
+//     document.querySelector('#section1').style.fontWeight = 'bold';
+// };
+
+// const makeBlue = () => {
+//     // your code here...
+//     console.log('Change background to blue');
+//     document.querySelector('body').style.backgroundColor = 'blue';
+//     document.querySelector('#section2').style.backgroundColor = 'blue';
+//     document.querySelector('#section2').style.fontWeight = 'bold';
+// };
+
+// const makePink = () => {
+//     // your code here...
+//     console.log('Change background to pink');
+//     document.querySelector('body').style.backgroundColor = 'pink';
+// };
+
+// const makeOrange = () => {
+//     // your code here...
+//     console.log('Change background to orange');
+//     document.querySelector('body').style.backgroundColor = 'orange';
+// };
+
+
